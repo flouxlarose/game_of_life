0e095fc9cf5b9106b858e404c4cfe7640d4989bc
eloi <eloivert@gmail.com>
2026-09-08 11:36:56 -0400

    modif

--- gol_engine.py
+++ gol_engine.py
@@ -88,22 +88,18 @@ class GOLEngine:
                 
                 self.__grille_futur[y][x] = next_status
 
-
-        for x in range(1, self.__width - 1):
-            for y in range(1, self.__height - 1):
-                neighbours: int = 0
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        if i != 0 or j != 0:
-                            neighbours += self.__current_state[x+i][y+j]
-                if self.__current_state[x][y] == 0:
-                    if neighbours == 3:
-                        self.__current_state[x][y] = 1
-                else:
-                    if neighbours == 2 or neighbours == 3:
-                        pass
-                    else:
-                        self.__current_state[x][y] = 0
+        # CODE DU PROF
+        # for x in range(1, self.__width - 1):
+        #     for y in range(1, self.__height - 1):
+        #         neighbours: int = 0
+        #         for i in range(-1, 2):
+        #             for j in range(-1, 2):
+        #                 if i != 0 or j != 0:
+        #                     neighbours += self.__current_state[x+i][y+j]
+        #         if self.__current_state[x][y] == 0: #mort
+        #             self.__new_state[x][y] = int(neighbours == 3)
+        #         else: # vivant
+        #             self.__new_state[x][y] = int(neighbours in (2,3))
 
 
         
